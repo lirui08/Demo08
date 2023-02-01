23550c8ef2aea829e6ea7f0ef0b2216a2a963d84
Rui <1071103616@qq.com>
2023-02-01 14:21:09 +0800

    toast

--- app/src/main/java/com/example/demo08/MainActivity.java
+++ app/src/main/java/com/example/demo08/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "新加的", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "新加的   dhfjk ", Toast.LENGTH_SHORT).show();
     }
 }
